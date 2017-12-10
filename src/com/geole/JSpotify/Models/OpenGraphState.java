cb9feda9760af248477d3ae1f4b012de6a274f85
George-Lewis <georgejeffreylewis@gmail.com>
2017-12-09 23:35:55 -0500

    Added missing values to models

--- src/com/geole/JSpotify/Models/OpenGraphState.java
+++ src/com/geole/JSpotify/Models/OpenGraphState.java
@@ -1,5 +1,20 @@
 package com.geole.JSpotify.Models;
 
 public class OpenGraphState {
+	
+	private final boolean posting_disabled, private_session;
+	
+	public OpenGraphState(boolean posting_disabled, boolean private_session) {
+		this.posting_disabled = posting_disabled;
+		this.private_session = private_session;
+	}
+
+	public boolean isPosting_disabled() {
+		return posting_disabled;
+	}
+
+	public boolean isPrivate_session() {
+		return private_session;
+	}
 
 }
--- src/com/geole/JSpotify/Models/SpotifyResource.java
+++ src/com/geole/JSpotify/Models/SpotifyResource.java
@@ -2,12 +2,24 @@ package com.geole.JSpotify.Models;
 
 public class SpotifyResource {
 	
-	private String name, location, uri;
+	private final String name, location, uri;
 	
 	public SpotifyResource(String name, String location, String uri) {
 		this.name = name;
 		this.location = location;
 		this.uri = uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getUri() {
+		return uri;
 	}
 
 }
