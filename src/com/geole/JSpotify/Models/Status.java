27d27e4ea09c602931a2c165bfb5f1856f152328
George-Lewis <georgejeffreylewis@gmail.com>
2018-01-07 14:01:06 -0500

    Javadoc and a small bugfix

--- src/com/geole/JSpotify/Models/Status.java
+++ src/com/geole/JSpotify/Models/Status.java
@@ -19,7 +19,7 @@ public class Status {
 	
 	public Status(JSONObject obj) {
 		this.server_time = obj.getLong("server_time");
-		this.playing_position = obj.getInt("playing_position");
+		this.playing_position = obj.optInt("playing_position", -1);
 		this.prev_enabled = obj.getBoolean("prev_enabled");
 		this.play_enabled = obj.getBoolean("play_enabled");
 		this.next_enabled = obj.getBoolean("next_enabled");
@@ -35,10 +35,16 @@ public class Status {
 		this.track = new Track(obj.getJSONObject("track"));
 	}
 
+	/**
+	 * @return The servers time as a unix timestamp in seconds
+	 */
 	public long getServerTime() {
 		return server_time;
 	}
 
+	/**
+	 * @return the playing position of the current song in seconds, may not always be present and will be indicated by a value of -1
+	 */
 	public int getPlayingPosition() {
 		return playing_position;
 	}
@@ -95,14 +101,30 @@ public class Status {
 		return track;
 	}
 	
+	/**
+	 * Convenience method to check if the currently playing song is by a particular artist
+	 * @param artist The artist name as seen on Spotify
+	 * @return true if the song playing is by that artist or false otherwise
+	 */
 	public boolean isListeningToArtist(String artist) {
 		return track.getArtist().getName().equalsIgnoreCase(artist);
 	}
 	
+	/**
+	 * Convenience method to check the currently playing song
+	 * @param song The name of the song as seen on Spotify
+	 * @return true if the song playing matches the supplied name
+	 */
 	public boolean isListeningToSong(String song) {
 		return track.getTrack().getName().equalsIgnoreCase(song);
 	}
 	
+	/**
+	 * Convenience method that checks the currently playing song and the artist
+	 * @param song The name of the song as seen on Spotify
+	 * @param artist The name of the artist as seen on Spotify
+	 * @return true if it's a match and false otherwise
+	 */
 	public boolean isListeningTo(String song, String artist) {
 		return track.getTrack().getName().equalsIgnoreCase(song) &&
 				track.getArtist().getName().equalsIgnoreCase(artist);
